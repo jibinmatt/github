c40740fb7a786f5f21878a3fd898cc281ca5259e
jibinmatt <jibinjtomas@gmail.com>
2023-07-07 15:14:21 +0530

    added sequential annimation to the minutes module

--- app.js
+++ app.js
@@ -2,59 +2,47 @@ const days = document.querySelector(".days")
 const hours = document.querySelector(".hours")
 const minutes = document.querySelector(".minutes")
 const seconds = document.querySelector(".seconds")
+const secondsFlip = document.querySelector(".seconds-flip:not([flip-above])");
+const minutesFlip = document.querySelector(".minutes-flip:not([flip-above])");
+const hoursFlip = document.querySelector(".hours-flip:not([flip-above])");
+const daysFlip = document.querySelector(".days-flip:not([flip-above])");
 
-// setTimeout(decrement(), 1000)
-
-/*
-function decrement() {
-    seconds--
-    seconds.innerHTML = seconds
-    if (seconds == 0) {
-        seconds.innerHTML = 59
-        minutes--
-        minutes.innerHTML = minutes
-        if (minutes == 0) {
-            minutes.innerHTML = 59
-            hours--
-            hours.innerHTML = hours
-            if (hours == 0) {
-                hours.innerHTML = 24
-                days--
-                days.innerHTML = days
-                if (days == 0) {
-                    clearInterval()
-                }
-            }
-        }
-    }
-}
-*/
-
-let countDownDate = new Date("Jul 6, 2023 16:37:52").getTime();
+let countDownDate = new Date("Jul 17, 2023 15:48:52").getTime();
 
 let myfunc = setInterval(function() {
 
     let currentTime = new Date().getTime();
-    let timeleft = countDownDate - currentTime;
+    let timeLeft = countDownDate - currentTime;
 
-    let daysC = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    let hoursC = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutesC = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    let secondsC = Math.floor((timeleft % (1000 * 60)) / 1000);
+    let daysC = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hoursC = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutesC = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let secondsC = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     days.innerHTML = formatNumber(daysC)
     hours.innerHTML = formatNumber(hoursC) 
     minutes.innerHTML = formatNumber(minutesC)
     seconds.innerHTML = formatNumber(secondsC)
 
-    if (timeleft < 0) {
+    if (timeLeft < 0) {
         clearInterval(myfunc);
-        days.innerHTML = "" 
-        hours.innerHTML = "" 
-        minutes.innerHTML = ""
-        seconds.innerHTML = ""
+        days.innerHTML = "00"
+        hours.innerHTML = "00" 
+        minutes.innerHTML = "00"
+        seconds.innerHTML = "00"
+    }
+
+    secondsFlip.classList.add("animation");
+    console.log(secondsC)
+
+    if (secondsC == 0) {
+        minutesFlip.classList.add("animation");
+    } else {
+        minutesFlip.classList.remove("animation");
     }
 
 }, 1000);
 
 let formatNumber = (num) => (num).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+
+
